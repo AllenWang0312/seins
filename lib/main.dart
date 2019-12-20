23e0491963263a1f6c5751e6cabd6513636c0a9e
wpc <1007991483@qq.com>
2019-12-20 17:50:04 +0800

    somechange

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,40 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:seins/utils/log_util.dart';
 import 'application.dart';
-import 'model/netease/my_app.dart';
 import 'model/netease/provider/play_list_model.dart';
 import 'model/netease/provider/play_songs_model.dart';
 import 'model/netease/provider/user_model.dart';
-//import 'model/plannet/my_app.dart';
 import 'package:seins/model/netease/route/routes.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-//  runApp(MyApp());
+//import 'model/netease/my_app.dart';
+//
+//void main() {
+////  runApp(MyApp());
+//  Router router = Router();
+//  Routes.configureRoutes(router);
+//  Application.router = router;
+//  Application.setupLocator();
+//  LogUtil.init(tag: 'NETEASE_MUSIC');
+////  AudioPlayer.logEnabled = true;
+//  Provider.debugCheckInvalidValueType = null;
+//  runApp(MultiProvider(
+//    providers: [
+//      ChangeNotifierProvider<UserModel>(
+//        builder: (_) => UserModel(),
+//      ),
+//      ChangeNotifierProvider<PlaySongsModel>(
+//        builder: (_) => PlaySongsModel()..init(),
+//      ),
+//      ChangeNotifierProvider<PlayListModel>(
+//        builder: (_) => PlayListModel(),
+//      ),
+//    ],
+//    child: MyApp(),
+//  ));
+//}
 
-  Router router = Router();
-  Routes.configureRoutes(router);
-  Application.router = router;
-  Application.setupLocator();
-  LogUtil.init(tag: 'NETEASE_MUSIC');
-//  AudioPlayer.logEnabled = true;
-  Provider.debugCheckInvalidValueType = null;
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider<UserModel>(
-        builder: (_) => UserModel(),
-      ),
-      ChangeNotifierProvider<PlaySongsModel>(
-        builder: (_) => PlaySongsModel()..init(),
-      ),
-      ChangeNotifierProvider<PlayListModel>(
-        builder: (_) => PlayListModel(),
-      ),
-    ],
-    child: MyApp(),
-  ));
+import 'model/plannet/my_app.dart';
+void main() {
+  runApp(MyApp());
 }
