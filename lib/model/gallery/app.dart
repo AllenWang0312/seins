2a2a00e16463ee4000e8890c6301cfc47e00fba9
wpc <1007991483@qq.com>
2020-03-13 17:46:10 +0800

    sc

---
+++ lib/model/gallery/app.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+class GalleryApp extends StatefulWidget{
+  const GalleryApp({
+    Key key,
+    this.updat
+})
+  final UpdateUrlFetcher updateUrlFetcher;
+  final bool enablePerFormanceOverlay;
+  final bool
+}
